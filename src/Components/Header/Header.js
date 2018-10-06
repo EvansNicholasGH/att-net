426ff94e7402ce68912e892a13152f27736a2181
Nicholas Evans <nevans8@masonlive.gmu.edu>
2018-10-06 03:43:36 -0400

    commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+ 
+export default (props) => {
+    return (
+        <div>           
+            <Button variant="contained" color="primary"> HEADER BUTTON 1 </Button>
+            <Button variant="contained" color="primary"> HEADER BUTTON 2 </Button>
+            <Button variant="contained" color="primary"> HEADER BUTTON 3 </Button>
+        </div>
+    )
+}
